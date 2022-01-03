32af6c4e779bafc71e461fb7a9a6ab84ceb77e21
ios-alice <apple12347878@gmail.com>
2022-01-03 23:28:52 +0900

    뷰에 커스텀 클래스 붙이기

---
+++ MyMemory/MemoCell.swift
@@ -0,0 +1,23 @@
+//
+//  memoCell.swift
+//  MyMemory
+//
+//  Created by dasom_dev on 2022/01/03.
+//
+
+import UIKit
+
+class MemoCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ MyMemory/MemoReadVC.swift
@@ -0,0 +1,12 @@
+//
+//  MemoReadVC.swift
+//  MyMemory
+//
+//  Created by dasom_dev on 2022/01/03.
+//
+
+import UIKit
+
+class MemoReadVC: UIViewController {
+    
+}
